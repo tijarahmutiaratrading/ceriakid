dc7b017717195091a4ca4fe4c9cda3cc6dcffc1a
Muhammad Firdaus Hamid <tijarahmutiaratrading@gmail.com>
2026-04-29 09:55:09 +0000

    File changes

--- src/pages/GamesList.jsx
+++ src/pages/GamesList.jsx
@@ -91,8 +91,8 @@ export default function GamesList() {
   }
 
   return (
-    <div className="min-h-screen bg-pattern">
-      <div className="max-w-lg mx-auto px-4 py-6 pb-24">
+    <div className="min-h-screen bg-pattern pb-32">
+      <div className="max-w-lg mx-auto px-4 py-6">
         {/* Header */}
         <Link to="/">
           <motion.button
