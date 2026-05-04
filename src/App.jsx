6cbb2265ef9fd19981e226e33ff3e1d0054ab7dc
Muhammad Firdaus Hamid <tijarahmutiaratrading@gmail.com>
2026-05-04 05:18:04 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
